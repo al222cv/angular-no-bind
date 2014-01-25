ef928f28aed2c76722eb63af748def1d8d5e408e
Andreas Larsson <andreas@andreas-UX32VD.(none)>
2014-01-25 13:44:07 +0100

    ADded directive

---
+++ angular-no-bind.js
@@ -0,0 +1,12 @@
+angular.module('noBind').directive('noBind', ['$timeout', function($timeout) {
+    return{
+    	restrict: 'AC',
+    	scope: true,
+    	link: function(scope, element){
+    		$timeout(function(){
+    			element.removeClass('ng-binding ng-scope');
+    			scope.$destroy
+    		}, 0);
+    	}
+    };
+});
